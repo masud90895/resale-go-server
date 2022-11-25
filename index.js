deee39df11e5c890fb5953f5535da054e92cafc8
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-11-25 20:46:27 +0600

    all buyer get and delete method added

--- index.js
+++ index.js
@@ -82,6 +82,10 @@ async function run() {
       const seller = await userCollection.find({role : "Seller"}).toArray()
       res.send(seller)
     })
+    app.get("/allUser",async (req, res) => {
+      const buyer = await userCollection.find({role : "Buyer"}).toArray()
+      res.send(buyer)
+    })
 
     app.delete("/allSeller/:id", async (req, res) => {
       const id = req.params.id;
